024c613050c9e9e63e4a624063d5549ee5497886
Mei.N <kanakae187@gmail.com>
2025-04-03 00:39:38 +0900

    ➕ upgrade:vuetify関連追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,36 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  css: ['vuetify/lib/styles/main.css'],
+
+  build: {
+    transpile: ['vuetify'],
+  },
+
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+    ssr: {
+      noExternal: ['vuetify']
+    }
+  },
+
+  // GitHub Pages用の設定
+  app: {
+    baseURL: '/nuxt-app/' // リポジトリ名に合わせて変更
+  },
+
+  nitro: {
+    preset: 'github-pages',
+    // CSSファイルの処理に関する問題を解決
+    esbuild: {
+      options: {
+        loaders: {
+          '.css': 'text'
+        }
+      }
+    }
+  },
+
+  compatibilityDate: '2025-04-03'
 })
