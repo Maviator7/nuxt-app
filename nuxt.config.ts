2804dee70e22e2daca01f6c86aac5aa05c9f4b8f
Mei.N <kanakae187@gmail.com>
2025-04-04 22:07:34 +0900

    🔧 feat: nuxt.config.tsのrouter設定を追加し、generateディレクトリをdocsに変更。index.vueにテスト用のdivを追加。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['vuetify-nuxt-module'],
-  app: {
-    baseURL: '/nuxt-app/', // リポジトリ名に合わせて変更
+  router: {
+    base: '/nuxt-app/', // リポジトリ名に合わせて変更
   },
   nitro: {
     preset: 'github-pages' // GitHub Pages用のプリセット
+  },
+  generate: {
+    dir: 'docs'
   }
 })
