13f2ba69c811f003ab92b0c5239c14107551e6ff
Mei.N <kanakae187@gmail.com>
2025-04-06 23:01:25 +0900

    ✨ feat:ヘッダー追加

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<script setup>
+  const items = [
+    { title: 'menu1' },
+    { title: 'menu2' },
+    { title: 'menu3' },
+    { title: 'menu4' },
+  ]
+</script>
+
+<template>
+  <div>
+    <v-toolbar title="ヘッダー">
+      <v-btn icon="mdi-magnify"></v-btn>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn icon="mdi-menu" variant="text" v-bind="props"></v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            :value="i"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
